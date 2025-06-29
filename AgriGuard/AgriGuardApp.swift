5f86b67f8f6aef2e632832cb20b5c2c4b60fd3fb
L <2944083996@qq.com>
2025-06-29 20:18:56 +0800

    Initial Commit

---
+++ AgriGuard/AgriGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgriGuardApp.swift
+//  AgriGuard
+//
+//  Created by mart S on 2025/6/29.
+//
+
+import SwiftUI
+
+@main
+struct AgriGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
